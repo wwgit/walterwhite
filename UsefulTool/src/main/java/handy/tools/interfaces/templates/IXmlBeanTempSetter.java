eddeeff0d5b1e8302deb33f23e89ec786e6cce13
wwgit <steveredd@sohu.com>
2017-01-04 23:29:08 +0800

    updates

---
+++ UsefulTool/src/main/java/handy/tools/interfaces/templates/IXmlBeanTempSetter.java
@@ -0,0 +1,17 @@
+package handy.tools.interfaces.templates;
+
+public interface IXmlBeanTempSetter {
+	
+	public void setXmlBeansTab(String XML_BEANS_TAB);
+	public void setXmlBeanTab(String XML_BEAN_TAB);		
+	public void setXmlAttriBeanIdTab(String XML_ATTRI_BEAN_ID_TAB);
+	public void setXmlAttriBeanClazzTab(String XML_ATTRI_BEAN_CLAZZ_TAB);
+	
+	//load property related template
+	public void setXmlPropertyTab(String XML_PROPERTY_TAB);
+	public void setXmlAttriPropertyNameTab(String XML_ATTRI_PROPERTY_NAME_TAB);
+	public void setXmlValueTab(String XML_VALUE_TAB);
+	public void setXmlRefBeanTab(String XML_REF_BEAN_TAB);
+	public void setXmlAttriRefBeanIdPropTab(String XML_ATTRI_REF_BEAN_ID_PROP_TAB);
+
+}
