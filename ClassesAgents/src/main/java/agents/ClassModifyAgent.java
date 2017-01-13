17a92015ca4694118043a6eb8ac700b600025f2d
wwgit <steveredd@sohu.com>
2017-01-13 14:42:47 +0800

    add agent for reloading and modifying classes at run-time

---
+++ ClassesAgents/src/main/java/agents/ClassModifyAgent.java
@@ -0,0 +1,33 @@
+/**   
+* @Title: ClassModifyAgent.java 
+* @Package agents 
+* @Description: TODO(what to do) 
+* @author walterwhite
+* @date 2017年1月13日 下午2:41:37 
+* @version V1.0   
+*/
+package agents;
+
+import java.lang.instrument.Instrumentation;
+
+/** 
+ * @ClassName: ClassModifyAgent 
+ * @Description: TODO(what to do) 
+ * @author walterwhite
+ * @date 2017年1月13日 下午2:41:37 
+ *  
+ */
+public class ClassModifyAgent {
+
+	/** 
+	 * @Title: main 
+	 * @Description: TODO(what to do) 
+	 * @param @param args  
+	 * @return void   
+	 * @throws 
+	 */
+	public static void premain(String agentArgs, Instrumentation _inst) {
+
+	}
+
+}
