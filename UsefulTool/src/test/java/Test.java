5fefd08e6c9cab556281907dacfadf067f00d322
wwgit <steveredd@sohu.com>
2017-01-13 14:34:31 +0800

    updates

---
+++ UsefulTool/src/test/java/Test.java
@@ -0,0 +1,19 @@
+/**   
+ * @Title: Test.java 
+ * @Package  
+ * @Description: TODO(what to do) 
+ * @author walterwhite
+ * @date 2017年1月13日 下午2:33:54 
+ * @version V1.0   
+ */
+
+/** 
+ * @ClassName: Test 
+ * @Description: TODO(what to do) 
+ * @author walterwhite
+ * @date 2017年1月13日 下午2:33:54 
+ *  
+ */
+public class Test {
+
+}
