9405780a04366e234facc4fb1527385886188199
wwgit <steveredd@sohu.com>
2017-01-26 14:41:54 +0800

    android instrument study

---
+++ FirstAndApp/src/com/example/firstandapp/beans/Student.java
@@ -0,0 +1,53 @@
+/**   
+* @Title: Students.java 
+* @Package com.example.firstandapp.beans 
+* @Description: TODO(what to do) 
+* @author walterwhite
+* @date 2017年1月26日 上午10:30:13 
+* @version V1.0   
+*/
+package com.example.firstandapp.beans;
+
+/** 
+ * @ClassName: Students 
+ * @Description: TODO(what to do) 
+ * @author walterwhite
+ * @date 2017年1月26日 上午10:30:13 
+ *  
+ */
+public class Student {
+	
+	private String name;
+	private String gender;
+	private int age;
+	private String address;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+
+}
